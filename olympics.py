91b8601a3377d2d1c6b6ccb5faba142a448c08ad
qiqi0211 <liqiqi244@gmail>
2025-12-18 09:23:08 -0500

    added minor fixes

--- olympics.py
+++ olympics.py
@@ -50,7 +50,7 @@ if not os.path.exists(LOCAL_PATH):
 # -----------------------------
 @st.cache_data
 def load_all_data():
-    conn = duckdb.connect(LOCAL_PATH, read_only=True)
+    conn = duckdb.connect(LOCAL_PATH)
     df = conn.execute("SELECT * FROM wiki_pageviews").df()
     conn.close()
     df["date"] = pd.to_datetime(df["date"], errors="coerce")
